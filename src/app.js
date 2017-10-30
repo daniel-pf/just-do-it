50d5e87cc7b2c9ea1118dcb0a32d9bb5a6410e71
Daniel Ferreira <daniel.ferreira@a7.net.br>
2017-10-30 08:02:41 -0200

    Adicionado Vue.js
    Tipo: I (C-Correção, I-Implementação, M-Melhoria)
    
    Descrição:
      - Corrigido algumas dependencias que havia esquecido se salvar.
      - Adicionado Vue.js no projeto.
    
    Observações:
      - Configuração baseado no projeto criado seguindo o seguinte tutorial:
        https://vue-loader.vuejs.org/en/start/setup.html
    
    Referências:

--- src/app.js
+++ src/app.js
@@ -1,4 +1,14 @@
 require('bootstrap');
 require('font-awesome-sass-loader');
+require('vue');
 const css = require('./base.scss');
 
+import Vue from 'vue';
+
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,26 @@ const webpack = require('webpack')
 const path = require('path');
 
 module.exports = {
-  entry:'./src/app.js',  
+  entry: './src/app.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [{
+      test: /\.vue$/,
+      loader: 'vue-loader',
+      options: {
+        loaders: {
+          // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+          // the "scss" and "sass" values for the lang attribute to the right configs here.
+          // other preprocessors should work out of the box, no loader config like this necessary.
+          'scss': 'vue-style-loader!css-loader!sass-loader',
+          'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+        }
+        // other vue-loader options go here
+      }
+    }, {
       test: /\.scss$/,
       use: [{
         loader: 'style-loader' // creates style nodes from JS strings
@@ -21,15 +34,20 @@ module.exports = {
           includePaths: ['node_modules']
         }
       }]
-    },
-    {
+    }, {
       test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
       loader: 'url-loader?limit=10000&mimetype=application/font-woff'
     }, {
       test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
       loader: 'file-loader'
-    }
-  ]},
+    }, {
+      test: /\.(png|jpg|gif|svg)$/,
+      loader: 'file-loader',
+      options: {
+        name: '[name].[ext]?[hash]'
+      }
+    }]
+  },
   plugins: [
     new CopyWebpackPlugin([
       { from: 'src/main.js' },
@@ -45,5 +63,44 @@ module.exports = {
       Util: "exports-loader?Util!bootstrap/js/dist/util",
       Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown"
     })
-  ]
+  ],
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true,
+    overlay: true
+  },
+  performance: {
+    hints: false
+  },
+  devtool: '#eval-source-map',
+  // TODO: Não é recomendado usar o compilador incluído, entender melhor as opções
+  // [Vue warn]: You are using the runtime-only build of Vue where the template option is not
+  // available. Either pre-compile the templates into render functions, or use the
+  // compiler-included build.
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.js'
+    }
+  }
+};
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
 };
